b2584b53987fae964706c7d4c31e745a95963338
Kavin K G <kavin@qpicloud.tech>
2021-12-18 20:44:02 +0530

    feat: ui for app and integration for tasks to be done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import PropTypes from "prop-types";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import TaskLists from "./components/TaskLists";
+import { styled } from "@mui/material/styles";
 
-function App() {
+const TabPanel = (props) => {
+  const { children, value, index, ...other } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   );
-}
+};
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+const Div = styled("div")(({ theme }) => ({
+  ...theme.typography.button,
+  backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(1),
+}));
+
+const App = () => {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm" sx={{ margin: "4% auto" }}>
+        <Paper elevation={4}>
+          <Box sx={{ width: "100%" }}>
+            <Div sx={{ display: "flex", justifyContent: "center" }}>
+              To-do App
+            </Div>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                textColor="secondary"
+                indicatorColor="secondary"
+                centered
+              >
+                <Tab id="todo" label="Tasks List" aria-label="todo" />
+                <Tab id="done" label="Completed" aria-label="done" />
+              </Tabs>
+            </Box>
+
+            <TabPanel value={value} index={0}>
+              <TaskLists />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              Work In Progess
+            </TabPanel>
+          </Box>
+        </Paper>
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default App;
---
+++ src/components/AddTask.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import axios from "axios";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+
+const AddTask = (props) => {
+  const [taskName, setTaskName] = useState("");
+  const [showSnackBar, setSnackBar] = useState(false);
+
+  const closeSnackBar = (event, reason) => {
+    if (reason === "clickaway") return;
+    setSnackBar(false);
+  };
+
+  const onAddTask = async () => {
+    try {
+      const response = await axios.post(`http://localhost:5000/task/create`, {
+        taskName,
+        status: "Active",
+      });
+      console.log(response);
+      if (response.data.status === "success") {
+        setSnackBar(true);
+        props.handleAddTask();
+        props.reload();
+      } else {
+        console.error("Error fetching data");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Dialog open={props.visible} onClose={props.handleAddTask}>
+        <DialogTitle>Add New Task</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="taskName"
+            label="Task Name"
+            type="text"
+            fullWidth
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.handleAddTask}>Cancel</Button>
+          <Button onClick={onAddTask}>Add Task</Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={showSnackBar}
+        onClose={closeSnackBar}
+        autoHideDuration={6000}
+        message={"Task Added Successfully!"}
+      />
+    </React.Fragment>
+  );
+};
+
+export default AddTask;
